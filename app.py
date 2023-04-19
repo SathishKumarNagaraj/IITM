4f3de45444cf20eb098ddaa24c52a14f71e27858
SathishKumarNagaraj <122247505+SathishKumarNagaraj@users.noreply.github.com>
2023-04-19 22:27:43 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 import streamlit as st
 
-def my_func(a, b, c):
+def my_func(a, b, c, reset=0):
     lis = [a, b, c]
     lis.sort()
+    if reset:
+        st.write("")
+        return
     out_str = "The largest number among the inputs is " + str(lis[-1])
     st.write(out_str)
 st.set_page_config(page_title="22f2001467", layout="wide")
@@ -14,4 +17,13 @@ first = st.number_input(label="Enter First Number")
 second = st.number_input(label="Enter Second Number")
 third = st.number_input(label="Enter Third Number")
 
-st.button("Find Largest", on_click=my_func(first, second, third))
+largest = st.button("Find Largest")
+if largest:
+    my_func(first, second, third)
+reset = st.button("Reset")
+if reset:
+    first = 0
+    second = 0
+    third = 0
+    my_func(first, second, third, 1)
+
